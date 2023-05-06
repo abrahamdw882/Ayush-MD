f75203546795bff506b681b49d3cbc9baf5ed643
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-05-07 02:21:01 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -551,3 +551,106 @@ module.exports = {
         await Miku.sendMessage(m.sender,{image:{url:botpic}, caption:txt},{quoted:m})
     }
 }
+
+//-------------------------------------------//
+
+const { exec } = require("child_process");
+const fs = require("fs");
+const { getRandom } = require("../../lib/myfunc");
+
+module.exports = {
+  name: "bass",
+  alias: ["bassboost"],
+  desc: "To bass boost a song",
+  category: "Audio Edit",
+  usage: "bass <reply to audio>",
+  react: "🍁",
+  start: async (Miku, m, { text, prefix, quoted, pushName, mime, body }) => {
+    let media = await Miku.downloadAndSaveMediaMessage(quoted);
+    let set = "-af equalizer=f=18:width_type=o:width=2:g=14";
+    let ran = getRandom(".mp3");
+    try {
+      exec(`ffmpeg -i ${media} ${set} ${ran}`, (err, stderr, stdout) => {
+        fs.unlinkSync(media);
+        if (err) return m.reply("An error Occurd !");
+        let buff = fs.readFileSync(ran);
+        Miku.sendMessage(
+          m.from,
+          { audio: buff, mimetype: "audio/mpeg" },
+          { quoted: m }
+        );
+        fs.unlinkSync(ran);
+      });
+    } catch (e) {
+      m.reply("An error Occurd ! Please mention an Audio!");
+    }
+  },
+};
+
+const { exec } = require("child_process");
+const fs = require("fs");
+const { getRandom } = require("../../lib/myfunc");
+
+module.exports = {
+  name: "blown",
+  alias: ["blowneffect"],
+  desc: "To add blown effect in a song",
+  category: "Audio Edit",
+  usage: "blown <reply to audio>",
+  react: "🍁",
+  start: async (Miku, m, { text, prefix, quoted, pushName, mime, body }) => {
+    let media = await Miku.downloadAndSaveMediaMessage(quoted);
+    let set = "-af acrusher=.1:1:40:0:log";
+    let ran = getRandom(".mp3");
+    try {
+      exec(`ffmpeg -i ${media} ${set} ${ran}`, (err, stderr, stdout) => {
+        fs.unlinkSync(media);
+        if (err) return m.reply("An error Occurd !");
+        let buff = fs.readFileSync(ran);
+        Miku.sendMessage(
+          m.from,
+          { audio: buff, mimetype: "audio/mpeg" },
+          { quoted: m }
+        );
+        fs.unlinkSync(ran);
+      });
+    } catch (e) {
+      m.reply("An error Occurd ! Please mention an Audio!");
+    }
+  },
+};
+
+const { exec } = require("child_process");
+const fs = require("fs");
+const { getRandom } = require("../../lib/myfunc");
+
+module.exports = {
+  name: "deep",
+  alias: ["deepeffect"],
+  desc: "To add deep reverb in a song",
+  category: "Audio Edit",
+  usage: "blown <reply to audio>",
+  react: "🍁",
+  start: async (Miku, m, { text, prefix, quoted, pushName, mime, body }) => {
+    let media = await Miku.downloadAndSaveMediaMessage(quoted);
+    let set = "-af atempo=4/4,asetrate=44500*2/3";
+    let ran = getRandom(".mp3");
+    try {
+      exec(`ffmpeg -i ${media} ${set} ${ran}`, (err, stderr, stdout) => {
+        fs.unlinkSync(media);
+        if (err) return m.reply("An error Occurd !");
+        let buff = fs.readFileSync(ran);
+        Miku.sendMessage(
+          m.from,
+          { audio: buff, mimetype: "audio/mpeg" },
+          { quoted: m }
+        );
+        fs.unlinkSync(ran);
+      });
+    } catch (e) {
+      m.reply("An error Occurd ! Please mention an Audio!");
+    }
+  },
+};
+
+
